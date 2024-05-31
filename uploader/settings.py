c2260121d5be4108b690a0c5748079c9f0d428d1
Vaibhav <vaibhavtiwary7766@gmail.com>
2024-05-31 12:33:58 +0530

    update database settings

--- uploader/settings.py
+++ uploader/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
 }
 database_url = os.environ.get("DATABASE_URL")
 
-DATABASES["default"] = dj_database_url.parse("database_url")
+DATABASES["default"] = dj_database_url.parse(database_url)
 
 
 # Password validation
